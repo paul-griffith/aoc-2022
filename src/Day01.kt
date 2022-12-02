e29cd53512f1f860a7434a247317332214c8bdfd
Paul Griffith <pgriffith@ia.io>
2022-12-02 09:43:57 -0800

    Day 2 (not good)

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,26 +1,26 @@
-fun caloriesByElf(input: Sequence<String>): List<Int> {
-    return buildList {
-        val currentElf = mutableListOf<Int>()
-        for (line in input) {
-            if (line.isBlank()) {
-                add(currentElf.sum())
-                currentElf.clear()
-            } else {
-                currentElf += line.toInt()
+fun main() {
+    fun caloriesByElf(input: Sequence<String>): List<Int> {
+        return buildList {
+            val currentElf = mutableListOf<Int>()
+            for (line in input) {
+                if (line.isBlank()) {
+                    add(currentElf.sum())
+                    currentElf.clear()
+                } else {
+                    currentElf += line.toInt()
+                }
             }
         }
     }
-}
 
-fun part1(caloriesByElf: List<Int>): Int {
-    return caloriesByElf.max()
-}
+    fun part1(caloriesByElf: List<Int>): Int {
+        return caloriesByElf.max()
+    }
 
-fun part2(caloriesByElf: List<Int>): Int {
-    return caloriesByElf.sortedDescending().take(3).sum()
-}
+    fun part2(caloriesByElf: List<Int>): Int {
+        return caloriesByElf.sortedDescending().take(3).sum()
+    }
 
-fun main() {
     val caloriesByElf = caloriesByElf(readInput("day01"))
 
     println(part1(caloriesByElf))
---
+++ src/Day02.kt
@@ -0,0 +1,105 @@
+import Outcome.Draw
+import Outcome.Lose
+import Outcome.Win
+import Hand.Paper
+import Hand.Rock
+import Hand.Scissors
+
+enum class Hand(val score: Int) {
+    Rock(1),
+    Paper(2),
+    Scissors(3);
+
+    companion object {
+        fun fromChar(char: Char) = when (char) {
+            'A', 'X' -> Rock
+            'B', 'Y' -> Paper
+            'C', 'Z' -> Scissors
+            else -> throw IllegalStateException()
+        }
+    }
+}
+
+enum class Outcome(val score: Int) {
+    Lose(0),
+    Draw(3),
+    Win(6);
+
+    override fun toString(): String = when (this) {
+        Win -> "beats"
+        Lose -> "loses to"
+        Draw -> "draws"
+    }
+
+    companion object {
+        fun fromChar(char: Char) = when (char) {
+            'X' -> Lose
+            'Y' -> Draw
+            'Z' -> Win
+            else -> throw IllegalStateException()
+        }
+    }
+}
+
+data class Round(val theirMove: Hand, val yourMove: Hand) {
+    constructor(theirChar: Char, yourChar: Char) : this(
+        Hand.fromChar(theirChar),
+        Hand.fromChar(yourChar)
+    )
+
+    private val outcome: Outcome = when {
+        theirMove == Rock && yourMove == Rock -> Draw
+        theirMove == Paper && yourMove == Rock -> Lose
+        theirMove == Scissors && yourMove == Rock -> Win
+        theirMove == Rock && yourMove == Paper -> Win
+        theirMove == Paper && yourMove == Paper -> Draw
+        theirMove == Scissors && yourMove == Paper -> Lose
+        theirMove == Rock && yourMove == Scissors -> Lose
+        theirMove == Paper && yourMove == Scissors -> Win
+        theirMove == Scissors && yourMove == Scissors -> Draw
+        else -> throw IllegalStateException("Impossible")
+    }
+
+    val score: Int = yourMove.score + outcome.score
+
+    override fun toString(): String {
+        return "$yourMove $outcome $theirMove - score: $score (${yourMove.score} + ${outcome.score})"
+    }
+}
+
+fun main() {
+    fun part1(input: Sequence<String>): Int {
+        return input.sumOf { line ->
+            val (theirMove, _, yourMove) = line.toCharArray()
+            val round = Round(theirMove, yourMove)
+//            println("$line - $round")
+            round.score
+        }
+    }
+
+    fun part2(input: Sequence<String>): Int {
+        return input.sumOf { line ->
+            val (theirMove, _, outcome) = line.toCharArray()
+            val theirHand = Hand.fromChar(theirMove)
+            val yourHand = when (Outcome.fromChar(outcome)) {
+                Lose -> when (theirHand) {
+                    Rock -> Scissors
+                    Paper -> Rock
+                    Scissors -> Paper
+                }
+                Draw -> theirHand
+                Win -> when (theirHand) {
+                    Rock -> Paper
+                    Paper -> Scissors
+                    Scissors -> Rock
+                }
+            }
+            val round = Round(theirHand, yourHand)
+//            println("$line - $round")
+            round.score
+        }
+    }
+
+    println(part1(readInput("day02")))
+    println(part2(readInput("day02")))
+}
