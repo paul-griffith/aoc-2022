93d485b3a05985a50ae147b48a303086d12a89cc
Paul Griffith <39345262+paul-griffith@users.noreply.github.com>
2022-12-04 01:34:50 +0000

    Day 3

---
+++ src/Day03.kt
@@ -0,0 +1,51 @@
+fun main() {
+    fun String.splitInHalf(): Pair<String, String> {
+        return Pair(this.substring(0, length / 2), this.substring(length / 2))
+    }
+
+    fun Char.score(): Int {
+        val base = this.lowercase().first().code - 96
+        return if (isUpperCase()) {
+            base + 26
+        } else {
+            base
+        }
+    }
+
+    fun part1(input: Sequence<String>): Int {
+        return input.sumOf { line ->
+            val (first, second) = line.splitInHalf()
+            println("$first $second")
+            val intersection = first.toSet().intersect(second.toSet()).first()
+            println(intersection.toString())
+            val result = intersection.score()
+            println(result.toString())
+            result
+        }
+    }
+
+    fun part2(input: Sequence<String>): Int {
+        return input.chunked(3).sumOf { group ->
+            println(group)
+            val common = group
+                .map { it.toSet() }
+                .reduce { acc, next ->
+                    acc intersect next
+                }.first()
+            println(common)
+            common.score()
+        }
+    }
+
+    // val sample = sequenceOf(
+    //     "vJrwpWtwJgWrhcsFMMfFFhFp",
+    //     "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+    //     "PmmdzqPrVvPwwTWBwg",
+    //     "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+    //     "ttgJtRGJQctTZtZT",
+    //     "CrZsJsPPZsGzwwsLwLmpwMDw",
+    // )
+
+    println(part1(readInput("day03")))
+    println(part2(readInput("day03")))
+}
