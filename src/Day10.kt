100a50e280e383b784c3edcf65b74935a92fdfa6
Paul Griffith <pgriffith@ia.io>
2022-12-15 12:53:07 -0800

    Day 10 - not great, not terrible

---
+++ src/Day10.kt
@@ -0,0 +1,190 @@
+fun main() {
+    fun part1(lines: Sequence<String>): Int {
+        return sequence {
+            var x = 1
+            var clock = 0
+
+            suspend fun SequenceScope<Int>.tick() {
+                clock += 1
+                val interestOffset = 20
+                val interestRepeat = 40
+
+                val col = (clock % 40) - 1
+                val range = (col - 1).coerceAtLeast(0)..(col + 1).coerceAtMost(40)
+                print(if (x in range) "#" else ".")
+                if (clock == interestOffset || (clock - interestOffset) % interestRepeat == 0) {
+                    val signal = x * clock
+                    yield(signal)
+                }
+                if (col + 1 == 0) {
+                    println()
+                }
+            }
+
+            for (line in lines) {
+                if (line == "noop") {
+                    tick()
+                } else {
+                    tick()
+                    tick()
+                    x += line.split(' ')[1].toInt()
+                }
+            }
+            println()
+        }.sum()
+    }
+
+    val sample = sequenceOf(
+        "addx 15",
+        "addx -11",
+        "addx 6",
+        "addx -3",
+        "addx 5",
+        "addx -1",
+        "addx -8",
+        "addx 13",
+        "addx 4",
+        "noop",
+        "addx -1",
+        "addx 5",
+        "addx -1",
+        "addx 5",
+        "addx -1",
+        "addx 5",
+        "addx -1",
+        "addx 5",
+        "addx -1",
+        "addx -35",
+        "addx 1",
+        "addx 24",
+        "addx -19",
+        "addx 1",
+        "addx 16",
+        "addx -11",
+        "noop",
+        "noop",
+        "addx 21",
+        "addx -15",
+        "noop",
+        "noop",
+        "addx -3",
+        "addx 9",
+        "addx 1",
+        "addx -3",
+        "addx 8",
+        "addx 1",
+        "addx 5",
+        "noop",
+        "noop",
+        "noop",
+        "noop",
+        "noop",
+        "addx -36",
+        "noop",
+        "addx 1",
+        "addx 7",
+        "noop",
+        "noop",
+        "noop",
+        "addx 2",
+        "addx 6",
+        "noop",
+        "noop",
+        "noop",
+        "noop",
+        "noop",
+        "addx 1",
+        "noop",
+        "noop",
+        "addx 7",
+        "addx 1",
+        "noop",
+        "addx -13",
+        "addx 13",
+        "addx 7",
+        "noop",
+        "addx 1",
+        "addx -33",
+        "noop",
+        "noop",
+        "noop",
+        "addx 2",
+        "noop",
+        "noop",
+        "noop",
+        "addx 8",
+        "noop",
+        "addx -1",
+        "addx 2",
+        "addx 1",
+        "noop",
+        "addx 17",
+        "addx -9",
+        "addx 1",
+        "addx 1",
+        "addx -3",
+        "addx 11",
+        "noop",
+        "noop",
+        "addx 1",
+        "noop",
+        "addx 1",
+        "noop",
+        "noop",
+        "addx -13",
+        "addx -19",
+        "addx 1",
+        "addx 3",
+        "addx 26",
+        "addx -30",
+        "addx 12",
+        "addx -1",
+        "addx 3",
+        "addx 1",
+        "noop",
+        "noop",
+        "noop",
+        "addx -9",
+        "addx 18",
+        "addx 1",
+        "addx 2",
+        "noop",
+        "noop",
+        "addx 9",
+        "noop",
+        "noop",
+        "noop",
+        "addx -1",
+        "addx 2",
+        "addx -37",
+        "addx 1",
+        "addx 3",
+        "noop",
+        "addx 15",
+        "addx -21",
+        "addx 22",
+        "addx -6",
+        "addx 1",
+        "noop",
+        "addx 2",
+        "addx 1",
+        "noop",
+        "addx -10",
+        "noop",
+        "noop",
+        "addx 20",
+        "addx 1",
+        "addx 2",
+        "addx 2",
+        "addx -6",
+        "addx -11",
+        "noop",
+        "noop",
+        "noop",
+    )
+
+//    println(part1(sample))
+    println(part1(readInput("day10")))
+//    println(part2(readInput("day07")))
+}
+
